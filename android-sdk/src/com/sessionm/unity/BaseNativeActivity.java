66a7e4d630fc8f15346c1a9778b8370367b4c657
Zhenhao Lei <zhenhaolei@sessionm.com>
2014-10-06 15:11:12 -0400

    Added android Unity plugin files

---
+++ android-sdk/src/com/sessionm/unity/BaseNativeActivity.java
@@ -0,0 +1,170 @@
+package com.sessionm.unity;
+
+import android.util.Log;
+import android.view.Menu;
+
+import com.sessionm.api.AchievementActivity;
+import com.sessionm.api.AchievementActivity.AchievementDismissType;
+import com.sessionm.api.AchievementActivityIllegalStateException;
+import com.sessionm.api.AchievementData;
+import com.sessionm.api.Activity;
+import com.sessionm.api.SessionM.ActivityType;
+import com.sessionm.api.SessionM;
+import com.unity3d.player.UnityPlayerActivity;
+
+public class BaseNativeActivity extends UnityPlayerActivity {
+
+    private final static String TAG = "SessionM.Unity";
+    
+    private final SessionM sessionM = SessionM.getInstance();
+    
+    public static final void setCallbackGameObjectName(String name) {
+        if(Log.isLoggable(TAG, Log.DEBUG)) {
+            Log.d(TAG, "Callback game object name: " + name);
+        }
+        SessionMListener.getInstance().setCallbackGameObjectName(name);
+    }
+   
+    // Convenience Unity/Android bridge methods
+    
+    public boolean presentActivity(ActivityType type) {
+        if(Log.isLoggable(TAG, Log.DEBUG)) {
+            Log.d(TAG, "Present activity: " + type);
+        }
+
+        return sessionM.presentActivity(type);
+    }
+    
+    public boolean isActivityAvailable(ActivityType type) {
+        boolean available = false;
+        if(sessionM.getSessionState() == SessionM.State.STARTED_ONLINE) {
+            if(type == ActivityType.ACHIEVEMENT) {
+                available = sessionM.getUnclaimedAchievement() != null;
+            } else {
+                available = true;
+            }
+        }
+        return available;
+    }
+    
+    public int getUnclaimedAchievementCount() {
+        return sessionM.getUser().getUnclaimedAchievementCount();
+    }
+
+    public String getUnclaimedAchievementJSON() {
+        String json = null;
+        AchievementData achievement = sessionM.getUnclaimedAchievement();
+        if(achievement != null) {
+            json = SessionMListener.getAchievementJSON(achievement);
+        }
+        return json;
+    }
+
+    public void notifyCustomAchievementPresented() {
+        AchievementData achievement = sessionM.getUnclaimedAchievement();
+        if(achievement == null) {
+            // this cannot happen 
+            Log.e(TAG, this + ".notifyCustomAchievementPresented(): Null achievement");
+            return;
+        }
+        
+        if(Log.isLoggable(TAG, Log.DEBUG)) {
+            Log.d(TAG, this + ".notifyCustomAchievementPresented(), achievement: " + achievement);
+        }
+        
+        AchievementActivity activity = new AchievementActivity(achievement);
+        try {
+            activity.notifyPresented();
+        } catch (AchievementActivityIllegalStateException e) {
+            Log.e(TAG, this + ".notifyCustomAchievementPresented()", e);
+        }
+    }
+    
+    public void notifyCustomAchievementCancelled() {
+        Activity activity = sessionM.getCurrentActivity();
+        if(Log.isLoggable(TAG, Log.DEBUG)) {
+            Log.d(TAG, this + ".notifyCustomAchievementCancelled(), activity: " + activity);
+        }
+
+        if(activity instanceof AchievementActivity) {
+            AchievementActivity a = (AchievementActivity)activity;
+            try {
+                a.notifyDismissed(AchievementDismissType.CANCELLED);
+            } catch (AchievementActivityIllegalStateException e) {
+                Log.e(TAG, this + ".notifyCustomAchievementCancelled()", e);
+            }
+        }
+    }
+
+    public void notifyCustomAchievementClaimed() {
+        Activity activity = sessionM.getCurrentActivity();
+        if(Log.isLoggable(TAG, Log.DEBUG)) {
+            Log.d(TAG, this + ".notifyCustomAchievementClaimed(), activity: " + activity);
+        }
+
+        if(activity instanceof AchievementActivity) {
+            AchievementActivity a = (AchievementActivity)activity;
+            try {
+                a.notifyDismissed(AchievementDismissType.CLAIMED);
+            } catch (AchievementActivityIllegalStateException e) {
+                Log.e(TAG, this + ".notifyCustomAchievementClaimed()", e);
+            }
+        }
+    }
+    
+    // Activity 
+    
+    @Override
+    protected void onStart() {
+        if(Log.isLoggable(TAG, Log.DEBUG)) {
+            Log.d(TAG, this + ".onStart()");
+        }
+
+        super.onStart();
+        sessionM.onActivityStart(this);
+    }
+
+    @Override
+    protected void onRestart() {
+        if(Log.isLoggable(TAG, Log.DEBUG)) {
+            Log.d(TAG, this + ".onRestart()");
+        }       
+        super.onRestart();
+    }
+
+    @Override
+    protected void onResume() {
+        if(Log.isLoggable(TAG, Log.DEBUG)) {
+            Log.d(TAG, this + ".onResume()");
+        }
+
+        super.onResume();
+        sessionM.onActivityResume(this);
+    }
+
+    @Override
+    protected void onPause() {
+        if(Log.isLoggable(TAG, Log.DEBUG)) {
+            Log.d(TAG, this + ".onPause()");
+        }
+
+        sessionM.onActivityPause(this);
+        super.onPause();
+    }
+
+    @Override
+    protected void onStop() {
+        if(Log.isLoggable(TAG, Log.DEBUG)) {
+            Log.d(TAG, this + ".onStop()");
+        }
+
+        super.onStop();
+        sessionM.onActivityStop(this);
+    }
+
+    @Override
+    public boolean onPrepareOptionsMenu(Menu menu) {
+        sessionM.dismissActivity();
+        return super.onPrepareOptionsMenu(menu);
+    }
+}
---
+++ android-sdk/src/com/sessionm/unity/SessionMPlugin.java
@@ -0,0 +1,139 @@
+package com.sessionm.unity;
+
+import android.util.Log;
+
+import com.sessionm.api.AchievementActivity;
+import com.sessionm.api.AchievementActivity.AchievementDismissType;
+import com.sessionm.api.AchievementActivityIllegalStateException;
+import com.sessionm.api.AchievementData;
+import com.sessionm.api.Activity;
+import com.sessionm.api.SessionM.ActivityType;
+import com.sessionm.api.SessionM;
+
+public class SessionMPlugin{
+
+    private final static String TAG = "SessionM.Unity";
+    
+    private final static SessionM sessionM = SessionM.getInstance();
+    private static android.app.Activity ac = com.unity3d.player.UnityPlayer.currentActivity; 
+    public static final void setCallbackGameObjectName(String name) {
+        if(Log.isLoggable(TAG, Log.DEBUG)) {
+            Log.d(TAG, "Callback game object name: " + name);
+        }
+        SessionMListener.getInstance().setCallbackGameObjectName(name);
+    }
+    
+    public static boolean isActivityAvailable(ActivityType type) {
+        boolean available = false;
+        if(sessionM.getSessionState() == SessionM.State.STARTED_ONLINE) {
+            if(type == ActivityType.ACHIEVEMENT) {
+                available = sessionM.getUnclaimedAchievement() != null;
+            } else {
+                available = true;
+            }
+        }
+        return available;
+    }
+    
+    public static int getUnclaimedAchievementCount() {
+        return sessionM.getUser().getUnclaimedAchievementCount();
+    }
+
+    public static String getUnclaimedAchievementJSON() {
+        String json = null;
+        AchievementData achievement = sessionM.getUnclaimedAchievement();
+        if(achievement != null) {
+            json = SessionMListener.getAchievementJSON(achievement);
+        }
+        return json;
+    }
+
+    public static void notifyCustomAchievementPresented() {
+        AchievementData achievement = sessionM.getUnclaimedAchievement();
+        if(achievement == null) {
+            // this cannot happen 
+            Log.e(TAG, ac + ".notifyCustomAchievementPresented(): Null achievement");
+            return;
+        }
+        
+        if(Log.isLoggable(TAG, Log.DEBUG)) {
+            Log.d(TAG, ac + ".notifyCustomAchievementPresented(), achievement: " + achievement);
+        }
+        
+        AchievementActivity activity = new AchievementActivity(achievement);
+        try {
+            activity.notifyPresented();
+        } catch (AchievementActivityIllegalStateException e) {
+            Log.e(TAG, ac + ".notifyCustomAchievementPresented()", e);
+        }
+    }
+    
+    public static void notifyCustomAchievementCancelled() {
+        Activity activity = sessionM.getCurrentActivity();
+        if(Log.isLoggable(TAG, Log.DEBUG)) {
+            Log.d(TAG, ac + ".notifyCustomAchievementCancelled(), activity: " + activity);
+        }
+
+        if(activity instanceof AchievementActivity) {
+            AchievementActivity a = (AchievementActivity)activity;
+            try {
+                a.notifyDismissed(AchievementDismissType.CANCELLED);
+            } catch (AchievementActivityIllegalStateException e) {
+                Log.e(TAG, ac + ".notifyCustomAchievementCancelled()", e);
+            }
+        }
+    }
+
+    public static void notifyCustomAchievementClaimed() {
+        Activity activity = sessionM.getCurrentActivity();
+        if(Log.isLoggable(TAG, Log.DEBUG)) {
+            Log.d(TAG, ac + ".notifyCustomAchievementClaimed(), activity: " + activity);
+        }
+
+        if(activity instanceof AchievementActivity) {
+            AchievementActivity a = (AchievementActivity)activity;
+            try {
+                a.notifyDismissed(AchievementDismissType.CLAIMED);
+            } catch (AchievementActivityIllegalStateException e) {
+                Log.e(TAG, ac + ".notifyCustomAchievementClaimed()", e);
+            }
+        }
+    }
+  
+    // Prime31 Shared Activity 
+    public static void onStart() {
+        if(Log.isLoggable(TAG, Log.DEBUG)) {
+            Log.d(TAG, ac + "SessionM.onStart()");
+        }
+        sessionM.onActivityStart(ac);
+    }
+
+    public static void onRestart() {
+        if(Log.isLoggable(TAG, Log.DEBUG)) {
+            Log.d(TAG, ac + "SessionM.onRestart()");
+        }
+    }
+
+    public static void onResume() {
+        
+        if(Log.isLoggable(TAG, Log.DEBUG)) {
+            Log.d(TAG, ac + "SessionM.onResume()");
+        }
+        setCallbackGameObjectName(ac.toString());
+        sessionM.onActivityResume(ac);
+    }
+
+    public static void onPause() {
+        if(Log.isLoggable(TAG, Log.DEBUG)) {
+            Log.d(TAG, ac + "SessionM.onPause()");
+        }
+        sessionM.onActivityPause(ac);
+    }
+
+    public static void onStop() {
+        if(Log.isLoggable(TAG, Log.DEBUG)) {
+            Log.d(TAG, ac + "SessionM.onStop()");
+        }
+        sessionM.onActivityStop(ac);
+    }
+}

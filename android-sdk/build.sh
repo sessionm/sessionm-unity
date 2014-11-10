4baf0bdb1b90b5428b51bfda39476a86ab2481bc
Zhenhao Lei <zhenhaolei@sessionm.com>
2014-11-10 16:54:01 -0500

    Added Android SDK path

--- android-sdk/build.sh
+++ android-sdk/build.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 source ../Config-SdkVersion
+source ../Config
 
 #curl -o libs/SessionM.zip https://content.sessionm.com/1/56/3/5249589/SessionM_android_v1.9.14.zip
 echo "Building SessionM-Unity-Android-Library"
 sed -e 's/$(UNITY_SDK_VERSION)/'${UNITY_SDK_VERSION}'/g' src/com/sessionm/unity/SessionMPlugin.java.in > src/com/sessionm/unity/SessionMPlugin.java
+export ANDROID_HOME=${ANDROID_SDK_PATH}
 ./gradlew build
 cp target/libs/android-sdk.jar target/SessionMUnity.jar

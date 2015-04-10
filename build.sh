cdf3b32b843a602e8df6101b52cc4d52123420ad
Tristan Daniels <tdaniels@sessionm.com>
2015-04-10 14:13:24 -0400

    Updated SampleApp to allow editing in Unity 5. Updated iOS library .meta file to automatically link AdSupport and ImageIO frameworks. Exporting packages with .meta files to ensure asset files are correctly replaced when re-importing the packages for Unity 5.

--- build.sh
+++ build.sh
@@ -5,8 +5,6 @@ DEST_PATH=${PWD}
 
 rm -rf build/
 mkdir build/
-mkdir Plugin_Shared/Assets/Plugins/iOS/
-mkdir Plugin_Shared/Assets/Plugins/Android/
 
 ##Build Android Plugin SDK##
 cp ${SESSIONM_ANDROID_JAR_PATH} android-sdk/libs/SessionM.jar
@@ -25,11 +23,16 @@ cd ..
 cp -r Plugin_Shared/ build/BaseActivity/
 cp -r Plugin_Shared/ build/Prime31/
 
-cp Plugin_BaseActivity/AndroidManifest.xml  build/BaseActivity/Assets/Plugins/Android
+cp Plugin_BaseActivity/AndroidManifest.xml build/BaseActivity/Assets/Plugins/Android
+cp Plugin_BaseActivity/AndroidManifest.xml.meta build/BaseActivity/Assets/Plugins/Android
 cp Plugin_BaseActivity/ISessionM_Android.cs build/BaseActivity/Assets/Plugins/SessionM
+cp Plugin_BaseActivity/ISessionM_Android.cs.meta build/BaseActivity/Assets/Plugins/SessionM
 
 cp Plugin_Prime31/AndroidManifest.xml ${PRIME31_JAR_PATH} build/Prime31/Assets/Plugins/Android
+cp Plugin_Prime31/AndroidManifest.xml.meta build/Prime31/Assets/Plugins/Android
+cp Plugin_Prime31/Prime31UnityActivity.jar.meta build/Prime31/Assets/Plugins/Android
 cp Plugin_Prime31/ISessionM_Android.cs build/Prime31/Assets/Plugins/SessionM
+cp Plugin_Prime31/ISessionM_Android.cs.meta build/Prime31/Assets/Plugins/SessionM
 
 cd build/BaseActivity
 echo "Building SessionM Unity BaseActivity Plugin..."
--- clean.sh
+++ clean.sh
@@ -7,7 +7,5 @@ rm -rf iOS-sdk/build/
 rm -rf iOS-sdk/Pods/
 rm -f iOS-sdk/Podfile.lock
 rm -f iOS-sdk/SessionM-Unity/SessionM_Unity.h
-rm -rf Plugin_Shared/Assets/Plugins/Android/
-rm -rf Plugin_Shared/Assets/Plugins/iOS/
 
 echo "Done."

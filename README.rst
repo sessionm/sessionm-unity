c97ea9a646c18f682feb77734a550a2b41b64e10
Tristan Daniels-Midman <tdaniels@sessionm.com>
2016-09-13 14:50:53 -0400

    Updated sample app to use 1.15.8 iOS SDK. Updated iOS build script and Pods linking. Bumped plugin version to 2.1.3

--- README.rst
+++ README.rst
@@ -9,7 +9,7 @@ License: MIT
 Usage
 #####
 
-Please see http://www.sessionm.com/documentation/unity-integration.php 
+Please see https://documentation.sessionm.com/sdk-integrations/unity-integration
 
 ###########
 Build Steps 
@@ -17,11 +17,11 @@ Build Steps
 
 1. In order to build this extension, the following programs and files must be available:
 
-* Unity (Tested with Unity 5.1.0p1)
-* XCode (Tested with XCode 6.3.1)
-* CocoaPods (Tested with CocoaPods 0.37.2 - install the Ruby gem by running ``gem install cocoapods``)
+* Unity (Tested with Unity 5.3.3f1)
+* XCode (Tested with XCode 8.0)
+* CocoaPods (Tested with CocoaPods 1.0.1 - install the Ruby gem by running ``gem install cocoapods``)
 * Android SDK (Tested with Android SDK version 21. Please add your Android SDK path in Config file and make sure you have Android SDK build tool version 21 downloaded. If you are familiar with Android development, feel free to change version number in ``android-sdk/build.gradle`` and ``android-sdk/AndroidManifest.xml``)
-* SessionM iOS and Android SDKs. Available for downloading from: http://www.sessionm.com/documentation/downloads.php
+* SessionM iOS and Android SDKs. Available for downloading from: https://documentation.sessionm.com/sdk-downloads
 * If you want to build Prime31 version of plugin, we assume that you have Prime31UnityActivity.jar ready. For more information on the Prime31 plugin, check their web site https://prime31.com, and instructions where to download the plugin can be found on their github account: https://gist.github.com/prime31/0908e6100d7e228f1add
 
 2. Create a Config file based on the provided Config.example. 
@@ -42,4 +42,4 @@ Build Steps
    to clean the build.
 
 Note:
-We only tested this build script on Mac OS.
+These build steps are intended for a Mac OS environment.
--- iOS-sdk/build.sh
+++ iOS-sdk/build.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 source ../Config-SdkVersion
 
+set -ex
+
 echo "Installing latest SessionM podspec"
 pod install
-perl -pi -e 's/ -ObjC//g' 'Pods/Target Support Files/Pods/Pods.debug.xcconfig'
-perl -pi -e 's/ -ObjC//g' 'Pods/Target Support Files/Pods/Pods.release.xcconfig'
+
+DEBUG_CONFIG="Pods/Target Support Files/Pods-SessionM-Unity/Pods-SessionM-Unity.debug.xcconfig"
+RELEASE_CONFIG="Pods/Target Support Files/Pods-SessionM-Unity/Pods-SessionM-Unity.release.xcconfig"
+perl -pi -e 's/ -ObjC//g' "${DEBUG_CONFIG}"
+printf "\nVALID_ARCHS = armv7 armv7s arm64 i386 x86_64" >> "${DEBUG_CONFIG}"
+perl -pi -e 's/ -ObjC//g' "${RELEASE_CONFIG}"
+printf "\nVALID_ARCHS = armv7 armv7s arm64 i386 x86_64" >> "${RELEASE_CONFIG}"
+
 sed -e 's/$(UNITY_SDK_VERSION)/'${UNITY_SDK_VERSION}'/g' SessionM-Unity/SessionM_Unity.h.in > SessionM-Unity/SessionM_Unity.h
 
 CONFIGURATION=Release
@@ -21,8 +29,7 @@ LIBRARY_FILE_PATH=${INSTALL_PATH}/${LIBRARY_FILE_NAME}
 
 echo "Building ${SCHEME}"
 xcodebuild -workspace ${WORKSPACE} -scheme ${SCHEME} -sdk iphoneos -configuration ${CONFIGURATION} -derivedDataPath ${INSTALL_PATH} > ${INSTALL_PATH}/device.log
-#xcodebuild -workspace ${WORKSPACE} -scheme ${SCHEME} -sdk iphonesimulator -configuration ${CONFIGURATION} ARCHS="i386 x86_64" -derivedDataPath ${INSTALL_PATH} > ${INSTALL_PATH}/sim.log
-#lipo -create -output ${LIBRARY_FILE_PATH} ${PRODUCTS_PATH}-iphoneos/${LIBRARY_FILE_NAME} ${PRODUCTS_PATH}-iphonesimulator/${LIBRARY_FILE_NAME} > ${INSTALL_PATH}/lipo.log
-lipo -create -output ${LIBRARY_FILE_PATH} ${PRODUCTS_PATH}-iphoneos/${LIBRARY_FILE_NAME} > ${INSTALL_PATH}/lipo.log
+xcodebuild -workspace ${WORKSPACE} -scheme ${SCHEME} -sdk iphonesimulator -configuration ${CONFIGURATION} ARCHS="i386 x86_64" PLATFORM_NAME="iphonesimulator" -derivedDataPath ${INSTALL_PATH} > ${INSTALL_PATH}/sim.log
+lipo -create -output ${LIBRARY_FILE_PATH} ${PRODUCTS_PATH}-iphoneos/${LIBRARY_FILE_NAME} ${PRODUCTS_PATH}-iphonesimulator/${LIBRARY_FILE_NAME} > ${INSTALL_PATH}/lipo.log
 
 echo "Build is under: ${LIBRARY_FILE_PATH}"
